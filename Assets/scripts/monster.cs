6bc96a24f23965e92d1705fbdd7c823eb267dcf9
EricSun21115 <49332784+EricSun21115@users.noreply.github.com>
2019-04-06 15:12:34 +0800

    Create monster.cs

---
+++ Assets/scripts/monster.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class monster : MonoBehaviour {
+    public float speed = 1f;
+    void Update()
+    {
+        Vector3 t_position = gameObject.transform.position;
+        t_position.x = t_position.x - (speed * Time.deltaTime);
+        gameObject.transform.position = t_position;
+    }
+}
