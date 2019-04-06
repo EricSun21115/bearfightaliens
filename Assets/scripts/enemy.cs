c5dfcfda37a8ab9a2e107e899ba624bd9d12b6a9
EricSun21115 <49332784+EricSun21115@users.noreply.github.com>
2019-04-06 15:12:25 +0800

    Create enemy.cs

---
+++ Assets/scripts/enemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour {
+    public GameObject enemy_Sprite;
+    
+	void Start () {
+		for(int i=0;i<200; i++)
+        {
+            Vector3 position = new Vector3(Random.Range(5.0f, 1000.0f), Random.Range(4.0f, -3.0f), 0);
+            GameObject enemy = Instantiate(enemy_Sprite, position, Quaternion.identity) as GameObject;
+            enemy.transform.Rotate(0, 180f, 0);
+        }
+
+	}
+    
+
+}
