c0e2e076a49178d1140960f3bd6eda70dfba340b
EricSun21115 <49332784+EricSun21115@users.noreply.github.com>
2019-04-06 15:12:48 +0800

    Create time.cs

---
+++ Assets/scripts/time.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using UnityEngine.UI; //使用Unity UI程式庫。
+   
+public class time : MonoBehaviour
+{
+    
+    int time_int = 100;
+
+    public Text time_UI;
+
+    void Start()
+    {
+
+        InvokeRepeating("timer", 1, 1);
+
+    }
+
+    void timer()
+    {
+
+        time_int -= 1;
+
+        time_UI.text = time_int + "";
+
+        if (time_int == 0)
+        {
+
+            time_UI.text = "YOU WIN";
+
+            CancelInvoke("timer");
+
+        }
+
+    }
+
+}
